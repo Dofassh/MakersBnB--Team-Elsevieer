074a778f532a3ae2905837b2f1a7faf05af60e4b
stacy1999 <anastasiapolinesi@gmail.com>
2021-03-03 15:57:35 +0000

    Updated database

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var { User } = require("../database");
 var router = express.Router();
 var bodyParser = require('body-parser');
+const app = express();
 
-router.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Line below allows use of res.body in line 23
 //router.use(express.json())
 /* GET home page. */
@@ -22,6 +24,7 @@ router.get("/", async function (req, res, next) {
   // }
 });
 
+
 // Fix this post request so that user's input is taken
 // action="/" method="POST" added to <form> index.ejs
 // router.post("/", async function (req, res) {
@@ -38,14 +41,23 @@ router.get("/", async function (req, res, next) {
 // })
 
 router.post('/', (req, res) => {
-  const { username, email, birthday, password } = req.body
-  return User.create( { username, email, birthday, password })
-  .then((user) => res.send(user))
-  .catch((err) => {
-    console.log('***There was an error creating user', JSON.stringify(user))
-    return res.status(400).send(err)
-  })
+  var usernamef = req.body.username;
+  var emailf = req.body.email;
+  var birthdayf = req.body.birthday;
+  var passwordf = req.body.password;
+  
+  const user = User.create({ username: usernamef, email: emailf, birthday: birthdayf, password: passwordf })
+  console.log( user instanceof User);
+  //const { username, email, birthday, password } = req.body
+  // return User.create( { username, email, birthday, password })
+  //  .then((user) => res.send(user))
+  //  .catch((err) => {
+  //    console.log('***There was an error creating user', JSON.stringify(user))
+  //   return res.status(400).send(err)
+  //  })
 });
 
 
+
 module.exports = router;
+
