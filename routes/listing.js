8050c5e5507aad6493ec713a30c10bc99d33b38a
stacy1999 <anastasiapolinesi@gmail.com>
2021-03-05 15:01:54 +0000

    Connected database to form

--- routes/listing.js
+++ routes/listing.js
@@ -1,16 +1,26 @@
 var express = require("express");
-var { User } = require("../database");
+var { Listing } = require("../database");
 var router = express.Router();
 var bodyParser = require('body-parser');
 const app = express();
 
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.get("/", async function (req, res, next) {
-    //var userCount = await User.count();
     res.render("listing", { title: "MakersBnB" });
 });
 
-
+router.post("/", (req, res) => {
+    const { name, description, price, availableFrom, availableTo } = req.body;
+    console.log(req.body);
+    const listing = Listing.create({ name, description, price, availableFrom, availableTo })
+      .then((listing) => res.redirect("/listing"))
+      .catch((err) => {
+        console.log("***There was an error creating user", JSON.stringify(user));
+        return res.status(400).send(err);
+      });
+  });
 
 
 
